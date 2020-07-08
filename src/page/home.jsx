5a6d89486f38304a7fd2aa5502b893f3e7a8ec7c
Amit Kuntal <amit.kuntal@loanframe.com>
2020-07-08 23:35:30 +0530

    Navigatio bar created

---
+++ src/page/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './../components/navbar';
+
+
+function Home() {
+  return ( 
+      <div className="mainbackgroundcolor">
+      <Navbar />
+      <div className="mainbackgroundcolor2">
+          </div>
+      </div>
+    
+  );
+}
+
+export default Home;
